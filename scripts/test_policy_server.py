b6aabc53e21df5d21132d15eaf23c3c55d3bbb09
Pranav Atreya <77517299+pranavatreya@users.noreply.github.com>
2025-09-16 17:28:09 -0700

    Update test policy server script

--- scripts/test_policy_server.py
+++ scripts/test_policy_server.py
@@ -8,18 +8,23 @@ from roboarena.policy import BasePolicy
 
 def _make_dummy_observation(server_config: policy_server.PolicyServerConfig) -> dict:
     obs = {}
+    if server_config.image_resolution is not None:
+        h, w = server_config.image_resolution
+    else:
+        h, w = 288, 512
     if server_config.needs_wrist_camera:
-        obs["observation/wrist_image_left"] = np.zeros((224, 224, 3), dtype=np.uint8)
+        obs["observation/wrist_image_left"] = np.zeros((h, w, 3), dtype=np.uint8)
         if server_config.needs_stereo_camera:
-            obs["observation/wrist_image_right"] = np.zeros((224, 224, 3), dtype=np.uint8)
+            obs["observation/wrist_image_right"] = np.zeros((h, w, 3), dtype=np.uint8)
     if server_config.n_external_cameras > 0:
         for i in range(server_config.n_external_cameras):
-            obs[f"observation/exterior_image_{i}_left"] = np.zeros((224, 224, 3), dtype=np.uint8)
+            obs[f"observation/exterior_image_{i}_left"] = np.zeros((h, w, 3), dtype=np.uint8)
             if server_config.needs_stereo_camera:
-                obs[f"observation/exterior_image_{i}_right"] = np.zeros((224, 224, 3), dtype=np.uint8)
+                obs[f"observation/exterior_image_{i}_right"] = np.zeros((h, w, 3), dtype=np.uint8)
     if server_config.needs_session_id:
-        obs["session_id"] = np.zeros(1, dtype=np.int32)
+        obs["session_id"] = "38DSA3hd3"
     obs["observation/joint_position"] = np.zeros(7, dtype=np.float32)
+    obs["observation/cartesian_position"] = np.zeros(6, dtype=np.float32)
     obs["observation/gripper_position"] = np.zeros(1, dtype=np.float32)
     obs["prompt"] = "test"
     return obs
@@ -27,7 +32,6 @@ def _make_dummy_observation(server_config: policy_server.PolicyServerConfig) -> 
 
 def test_policy_server():
     client = WebsocketClientPolicy()
-    client.reset()
 
     # Make sure metadata is reasonable
     metadata = client.get_server_metadata()
@@ -47,6 +51,9 @@ def test_policy_server():
     assert isinstance(actions, np.ndarray)
     assert len(actions.shape) == 2
     assert actions.shape[-1] == 8
+
+    # At the end of the trajectory, reset will be called
+    client.reset()
 
     logging.info("Test passed")
 
